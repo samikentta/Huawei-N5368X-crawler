ff7f1ddb56f5d1c461b7c6a881e66c2eb2a270ff
samikentta <sami@kentta.net>
2021-01-25 22:55:24 +0200

    Update huawei-n5368x-crawler.py
    Support for HTTP POST to fetch e.g. modem IMEI, IMSI, serial number and traffic data. Some other tuning as well, e.g. changed how token are handled.

--- huawei-n5368x-crawler.py
+++ huawei-n5368x-crawler.py
@@ -5,26 +5,35 @@ import hashlib
 import hmac
 import json
 import datetime
+
+import base64
+
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+import binascii 
 from time import sleep
 from binascii import hexlify
 import requests
 from influxdb import InfluxDBClient
 from flatten_json import flatten 
 
+token = []
 time = datetime.datetime.utcnow()
 
 # Settings 
-ROUTER="192.168.8.1"   		# Router IP address
+ROUTER="192.168.1.1"   		# Router IP address
 USER="Admin"			# Router username
 PASSWORD=b"password_here"	# Router password
-USEINFLUX=False		        # Whether to use Influx DB or not, set True or False 
+USEINFLUX=True			# Whether to use Influx DB or not, set True or False 
 
 
 # Setup Influx database details in case that is used
 if USEINFLUX:
-    ifdb = "database_name"		# Database name
-    ifhost = "database_ip_address"	# Database server IP address
-    ifport = "8086"			# Database portnumber, 8086 by default
+    ifdb = "huawei"
+    ifhost = "192.168.8.202"
+    ifport = "8086"
 
 # Generate clientside nonce
 def generate_nonce():
@@ -39,9 +48,19 @@ def setup_session(client, server):
 
 # Get server token
 def get_server_token(client, server):
-    url = "http://%s/api/web/crsf_token" % server
-    token_response = client.get(url).text
-    root = json.loads(token_response)["tokens"].split(',')
+    global token
+    token.pop(0)
+    if len(token)<1:
+    	url = "http://%s/api/web/crsf_token" % server
+    	token_response = client.get(url).text
+    	token = json.loads(token_response)["tokens"].split(',')
+    return token[0] 
+
+def get_pubkey(client, server, cookies):
+    url = "http://%s/api/web/pubkey" % server
+    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8','requestverificationtoken': get_server_token(client, server) } 
+    pubkey_response = client.get(url, headers=headers, cookies=cookies).text
+    root = json.loads(pubkey_response)["pubkey"]
     return root
 
 # Calculate server-client proof, part of SCRAM algorithm
@@ -76,29 +95,76 @@ def influxwrite(result, apiurl):
 		"fields": fields
 	}
     ]
+    print(body)
     ifclient = InfluxDBClient(ifhost, ifport)
     ifclient.switch_database(ifdb)
     ifclient.write_points(body)
 
 # Calling router API
-def apicall(server, client, apiurl, tokeni, cookies): 
+def getapicall(server, client, apiurl, cookies): 
     print(apiurl)
     url = "http://%s/api/%s" % (server,apiurl)
-    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-               'requestverificationtoken': tokeni}
+    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'requestverificationtoken': get_server_token(client, server) }
     result = client.get(url, headers=headers, cookies=cookies).text
     print(result)
     sleep(1)
     if USEINFLUX: 
     	influxwrite(result,apiurl)
 
+def postpayload(apiurl):
+    if apiurl == 'modemmng/queryModemMonitorWithName':
+    	payload = {
+    		'monitorName': 'dataFlow',
+    		'argJson': { }
+    	}
+    elif apiurl == 'modemmng/queryModemIMSI':
+        payload = {
+                'monitorName': 'protocolStatus',
+                'argJson': { 'cmd': 'IMSI' }
+        }
+
+    elif apiurl == 'modemmng/queryModemIMEI':
+        payload = {
+                'monitorName': 'protocolStatus',
+                'argJson': { 'cmd': 'IMEI'}
+        }
+    elif apiurl == 'equipservice/getequippara':
+        payload = {
+                'ParaName': 'SNEX'
+        }
+ 
+    return payload
+
+# POST to router API
+def postapicall(server, client, apiurl, cookies):
+    pubkey = get_pubkey(client, server, cookies)
+    # print(pubkey)
+
+    payloadstr = json.dumps(postpayload(apiurl)).encode('utf8')
+    key = RSA.import_key(pubkey)
+    encryptor = PKCS1_OAEP.new(key)
+    encrypted = encryptor.encrypt(payloadstr)
+
+    base64_bytes = base64.b64encode(encrypted)
+    base64_message = base64_bytes.decode('utf-8')
+
+    headers = {'requestverificationtoken': get_server_token(client, server) }
+    url = "http://%s/api/%s" % (server,apiurl)
+    result = client.post(url, data=base64_message, headers=headers, cookies=cookies).text
+    if USEINFLUX:
+        influxwrite(result, apiurl)
+    print(result)
+
+
 # Login to router using SCRAM and fetch wanted data
 def login(client, server, user, password):
     # Setup session
     setup_session(client, server)
+    global token
+    token.append("INITIALIZE")
+
 
     # Get server token
-    token = get_server_token(client, server)
 
     # Collect login challenge
     url = "http://%s/api/login/login_challenge" % server
@@ -108,7 +174,7 @@ def login(client, server, user, password):
 	'username': user,
 	'firstnonce': firstnonce
     } 
-    headers = {'requestverificationtoken': token[0]}
+    headers = {'requestverificationtoken': get_server_token(client, server) }
     response = client.post(url, data=json.dumps(payload), headers=headers)
     servernonce = json.loads(response.text)["servernonce"]
     salt = json.loads(response.text)["salt"]
@@ -123,32 +189,41 @@ def login(client, server, user, password):
 	'clientproof':clientproof,
 	'finalnonce':servernonce
     }
-    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-               'requestverificationtoken': token[1]}
+    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'requestverificationtoken': get_server_token(client, server)}
     url = "http://%s/api/login/login_auth" % server
     response2 = client.post(url, data=json.dumps(payload), headers=headers, cookies=response.cookies)
 
     # Update token 
-    token = get_server_token(client, server)
 
     # Finalize login
     payload ={
 	'status': '0'
     }
-    headers = {'requestverificationtoken': token[0]}
+    headers = {'requestverificationtoken': get_server_token(client, server) }
     url = "http://%s/api/login/login_done" % server
     response2 = client.post(url, data=json.dumps(payload), headers=headers, cookies=response2.cookies)
 
+    # Reset tokens
+    del token
+    token = []
+    token.append("INITIALIZE")
+
     # Fetching data
-    apicall(server, client, 'modemmng/getAntennaConfiguration', token[1], response2.cookies)
-    apicall(server, client, 'modemmng/getSignal', token[2], response2.cookies)
-    apicall(server, client, 'modemmng/getNrAirStat', token[3], response2.cookies)
-    apicall(server, client, 'signalmng/getsiglevel', token[4], response2.cookies)
-    apicall(server, client, 'web/uptime', token[5], response2.cookies)
+    getapicall(server, client, 'modemmng/getAntennaConfiguration', response2.cookies)
+    getapicall(server, client, 'modemmng/getSignal', response2.cookies)
+    getapicall(server, client, 'modemmng/getNrAirStat', response2.cookies)
+    getapicall(server, client, 'signalmng/getsiglevel', response2.cookies)
+    getapicall(server, client, 'web/uptime', response2.cookies)
+    getapicall(server, client, 'device/version', response2.cookies)
     
+    postapicall(server, client, 'modemmng/queryModemMonitorWithName', response2.cookies)
+    postapicall(server, client, 'modemmng/queryModemIMEI', response2.cookies)
+    postapicall(server, client, 'modemmng/queryModemIMSI', response2.cookies)
+    postapicall(server, client, 'equipservice/getequippara', response2.cookies)
+ 
     # Logout
     payload ={'status': '0'}
-    headers = {'requestverificationtoken': token[6]}
+    headers = {'requestverificationtoken': get_server_token(client, server) }
     url = "http://%s/api/login/login_out" % server
     response2 = client.post(url, data=json.dumps(payload), headers=headers, cookies=response2.cookies)
 
