8cf8f4c5873c42fad1b7611465f51f086fa6b805
samikentta <sami@kentta.net>
2021-03-25 18:58:33 +0200

    Updated logging, other cleanups

--- v2/huawei-n5368x-crawler-v2.py
+++ v2/huawei-n5368x-crawler-v2.py
@@ -8,6 +8,7 @@ import re
 import datetime
 import logging
 import configparser
+import xml.etree.ElementTree as ET
 
 import base64
 
@@ -22,7 +23,8 @@ import requests
 from influxdb import InfluxDBClient
 from flatten_json import flatten 
 
-import xml.etree.ElementTree as ET
+from logging.config import fileConfig
+fileConfig('huawei.ini')
 
 config = configparser.ConfigParser()
 config.read('huawei.ini')
@@ -72,28 +74,17 @@ def influxwrite(result, apiurl):
 		"fields": fields
 	}
     ]
-    logging.info(body)
     if config.getboolean('influx','useinflux'):
+       logging.debug('Data to be sent to InfluxDB:' + str(body))
        ifclient = InfluxDBClient(config['influx']['ifhost'], config['influx']['ifport'])
        ifclient.switch_database(config['influx']['ifdb'])
        ifclient.write_points(body)
 
-# Calling router API
-#def getapicall(server, client, apiurl, cookies): 
-#    print(apiurl)
-#    url = "http://%s/api/%s" % (server,apiurl)
-#    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'requestverificationtoken': get_server_token(client, server) }
-#    result = client.get(url, headers=headers, cookies=cookies).text
-#    print(result)
-#    sleep(1)
-#    if USEINFLUX: 
-#    	influxwrite(result,apiurl)
-
 # Login to router using SCRAM and fetch wanted data
 def login(client, server, user, password):
     # Setup session
+    logging.info('Creating session')
     setup_session(client, server)
-
     url = "http://%s/" % server
     response = client.get(url) 
 
@@ -108,9 +99,9 @@ def login(client, server, user, password):
 
     root = ET.fromstring(response.text)
     token = root[0].text
-    logging.debug(token)
+    logging.debug('Server token: ' + token)
 
-
+    logging.info('Collect login challenge')
     # Collect login challenge
     url = "http://%s/api/user/challenge_login" % server
     clientnonce = generate_nonce()
@@ -118,30 +109,31 @@ def login(client, server, user, password):
     username = ET.SubElement(request, 'username')
     firstnonce = ET.SubElement(request, 'firstnonce')
     mode = ET.SubElement(request, 'mode')
-    username.text = 'Admin'
+    username.text = config['modem']['user'] 
     firstnonce.text = clientnonce 
     mode.text = '1'	
 
-    print (ET.tostring(request))
+    logging.debug(ET.tostring(request))
     headers = {'__RequestVerificationToken': token[32:64] }
     response = client.post(url, data=ET.tostring(request), headers=headers, cookies = response.cookies)
 
-    logging.debug(response.text)
+    logging.debug('Login challenge response: ' + response.text)
 
     salt = ET.fromstring(response.text)[0].text
     servernonce = ET.fromstring(response.text)[2].text
-    iterations = ET.fromstring(response.text)[4].text
+    iterations = str(ET.fromstring(response.text)[4].text)
 
-    logging.debug(salt)
-    logging.debug(servernonce)
-    logging.debug(iterations)
+    logging.debug('Salt: ' + salt)
+    logging.debug('Servernonce: ' + servernonce)
+    logging.debug('Iterations: ' + iterations)
 
- 
     # Get client proof 
+    logging.info('Calculate client proof')
     clientproof = get_client_proof(
-        clientnonce, servernonce, password, salt, 100).decode('UTF-8')
+        clientnonce, servernonce, password, salt, int(iterations)).decode('UTF-8')
     logging.debug(clientproof)
     # Authenticate
+    logging.info('Authenticate login')
     headers = {'__RequestVerificationToken': response.headers['__RequestVerificationToken']}
     url = "http://%s/api/user/authentication_login" % server
     request = ET.Element('request')
@@ -150,10 +142,10 @@ def login(client, server, user, password):
     xclientproof.text = clientproof
     xfinalnonce.text = servernonce 
 
-    logging.debug(ET.tostring(request))
+    logging.debug('Request form data: ' + str(ET.tostring(request)))
     response = client.post(url, data=ET.tostring(request), headers=headers, cookies=response.cookies)
 
-    logging.debug(response.text)
+    logging.debug('Authenticate response: ' + response.text)
 
 
     logoutrequestverificationtoken = response.headers['__RequestVerificationTokenone']
@@ -162,14 +154,14 @@ def login(client, server, user, password):
     logging.debug(response.text)
 
     for sections in config['useapi']:
-        print (sections)
+        logging.debug('Looping APIs: ' + sections)
 	
         api = config[sections]['api']    
-        logging.info('Getting ' + api)  
+        logging.info('Getting data from API: ' + api)  
         url = "http://%s/api/%s" % (server, api)
         response = client.get(url, cookies=response.cookies)
-        logging.debug(url)
-        logging.debug(response.text)
+        logging.debug('URL to fetch data: ' + url)
+        logging.debug('Response from router API: ' + response.text)
  
         if (config.getboolean('useapi', sections)):
             itersource = [e.strip() for e in config.get(sections, 'sourcefields').split(',')] 
@@ -183,16 +175,18 @@ def login(client, server, user, password):
                     if j:
                        iterresult += ", "
                     if itertypes[iteridx] == 'integer':
-                       logging.info(itertarget[iteridx] + ' is an integer')
+                       logging.debug(itertarget[iteridx] + ' is an integer')
                        itemtxt = int(float(re.sub("[\<\=a-zA-Z]", "", iteritem.text)))
                        iterresult += '"'+ itertarget[iteridx]+'": '+ str(itemtxt) 
+                       logging.info('Found value ' + itertarget[iteridx] + ': ' + str(itemtxt)) 
                     else :
-                       logging.info(itertarget[iteridx] + ' is a string')
+                       logging.debug(itertarget[iteridx] + ' is a string')
                        itemtxt = iteritem.text
                        iterresult += '"'+ itertarget[iteridx]+'": "'+ itemtxt + '"' 
+                       logging.info('Found value ' + itertarget[iteridx] + ': ' + itemtxt) 
                     j = 1
             iterresult += ' }' 
-            logging.debug(iterresult)
+            logging.debug('Result in JSON to be passed forward: ' + iterresult)
             influxwrite(iterresult, api) 
 
 
@@ -200,6 +194,7 @@ def login(client, server, user, password):
     request = ET.Element('request')
     xmllogout= ET.SubElement(request, 'Logout')
     xmllogout.text='1'
+    logging.info('Logging out')
     headers = {'__RequestVerificationToken': logoutrequestverificationtoken }
  
     response = client.post(url, data=ET.tostring(request), headers=headers, cookies = response.cookies)
@@ -211,7 +206,8 @@ def login(client, server, user, password):
 def main():
     """ main method """
 
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logger = logging.getLogger()
+   # logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     
     client = requests.Session()
     login(client, config['modem']['ipaddress'], config['modem']['user'], str.encode(config['modem']['password']))
